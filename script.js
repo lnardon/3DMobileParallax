cfa984f17b1f8214c722c9fb6d16fdf4db5029cd
unknown <lucaslnardon@gmail.com>
2023-04-28 21:10:34 -0800

    Change glb file and interaction

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// IMPORTS
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js";
 import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/loaders/GLTFLoader.js";
-import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js";
 
 //SCENE
 const scene = new THREE.Scene();
@@ -22,10 +20,8 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   3000
 );
-camera.position.z = 65;
-
-// CONTROLS
-const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.y = 1
+camera.position.z = 15;
 
 //LIGHTS
 const spotLight = new THREE.SpotLight(0xffffff);
@@ -50,7 +46,7 @@ scene.add(spotLight2);
 // Load gltf scene
 const loader = new GLTFLoader();
 loader.load(
-  "./Shapes.glb",
+  "./shapes.glb",
   function (gltf) {
     scene.add(gltf.scene);
   },
@@ -62,34 +58,45 @@ loader.load(
   }
 );
 
+//RENDER LOOP
+requestAnimationFrame(render);
+function render() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+
+
+
 function lerp (start, end, amt){
   return (1-amt)*start+amt*end
 }
 
 function handleParallax(x, y, z) {
   if(Math.abs(x) > 2){
-    scene.children[2].children[0].position.x = lerp(scene.children[2].children[0].position.x,scene.children[2].children[0].position.x + (x/21), 0.01)
+    scene.children[2].children.forEach(child => {
+      child.position.x = lerp(child.position.x,child.position.x + (x/14), 0.01)
+    });
   }
   if(Math.abs(y) > 2){
-    scene.children[2].children[0].position.y = lerp(scene.children[2].children[0].position.y,scene.children[2].children[0].position.y + (y/19), 0.03)
+    scene.children[2].children.forEach(child => {
+      child.position.y = lerp(child.position.y,child.position.y + (y/14), 0.01)
+    });
   }
   if(Math.abs(z) > 2){
-    scene.children[2].children[0].position.z = lerp(scene.children[2].children[0].position.z,scene.children[2].children[0].position.z + (z/23), 0.01)
+    scene.children[2].children.forEach(child => {
+      child.position.z = lerp(child.position.z,child.position.z + (z/14), 0.01)
+    });
   }
 }
 
-//RENDER LOOP
-requestAnimationFrame(render);
-function render() {
-  controls.update();
-  renderer.render(scene, camera);
-  requestAnimationFrame(render);
+if(window.DeviceMotionEvent){
+  window.addEventListener("devicemotion", (e) => {
+    // document.getElementById("logs").innerText = `${e.rotationRate.alpha} - ${e.rotationRate.beta} - ${e.rotationRate.gamma}`
+    handleParallax(e.rotationRate.beta,e.rotationRate.gamma,e.rotationRate.alpha);
+  });
+} else {
+  alert("Your device does not support the Device Motion Event.")
 }
-
-window.addEventListener("devicemotion", (e) => {
-  // document.getElementById("logs").innerText = `${e.rotationRate.alpha} - ${e.rotationRate.beta} - ${e.rotationRate.gamma}`
-  handleParallax(e.rotationRate.beta,e.rotationRate.alpha,e.rotationRate.gamma);
-});
 
 window.addEventListener(
   "resize",
